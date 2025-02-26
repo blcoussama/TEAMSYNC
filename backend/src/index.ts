af6e0c9f0603536b6cfabadd0a6654dd7ebc0dda
blcoussama <belcadioussama.eng@gmail.com>
2025-02-26 11:58:50 +0000

    CONFIGURED ALL the Project ENDPOINTS & Tested Successfully

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,6 +14,7 @@ import userRoutes from "./routes/User.ROUTE";
 import isAuthenticated from "./middlewares/IsAuthenticated.MIDDLEWARE";
 import workspaceRoutes from "./routes/Workspace.ROUTE";
 import memberRoutes from "./routes/Member.ROUTE";
+import projectRoutes from "./routes/Project.ROUTE";
 
 
 const app = express();
@@ -59,6 +60,7 @@ app.use(`${BASE_PATH}/auth`, authRoutes);
 app.use(`${BASE_PATH}/user`, isAuthenticated, userRoutes);
 app.use(`${BASE_PATH}/workspace`, isAuthenticated, workspaceRoutes);
 app.use(`${BASE_PATH}/member`, isAuthenticated, memberRoutes);
+app.use(`${BASE_PATH}/project`, isAuthenticated, projectRoutes);
 
 app.use(errorHandler);
 
---
+++ backend/src/routes/Project.ROUTE.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { createProjectController, deleteProjectController, getAllProjectsInWorkspaceController, getProjectAnalyticsController, getProjectByIdAndWorkspaceIdController, updateProjectController } from "../controllers/Project.CONTROLLER";
+
+
+const projectRoutes = Router();
+
+projectRoutes.post("/workspace/:workspaceId/create", createProjectController);
+
+projectRoutes.put("/:id/workspace/:workspaceId/update", updateProjectController);
+
+projectRoutes.delete("/:id/workspace/:workspaceId/delete", deleteProjectController);
+
+projectRoutes.get("/workspace/:workspaceId/all", getAllProjectsInWorkspaceController);
+
+projectRoutes.get("/:id/workspace/:workspaceId/analytics", getProjectAnalyticsController);
+
+projectRoutes.get("/:id/workspace/:workspaceId", getProjectByIdAndWorkspaceIdController);
+
+export default projectRoutes;
